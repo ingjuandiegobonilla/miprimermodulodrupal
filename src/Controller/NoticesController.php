390343a55eb18ec4f78ceb5794d52c0765556a36
zartos <daikerumaru@hotmail.com>
2019-04-04 17:18:37 -0500

    Se creo el codigo del controlador'

--- src/Controller/NoticesController.php
+++ src/Controller/NoticesController.php
@@ -1,5 +1,20 @@
 <?php
 
+namespace Drupal\notices\Controller;
+
+class NoticesController {
+
+    public function description(){
+        $build = array(
+            '#type' => 'markup',
+            '#markup' => '<p>'.t('Hola Mundo').'</p>',
+        );
+        
+        return $build;
+}
+}
+
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
