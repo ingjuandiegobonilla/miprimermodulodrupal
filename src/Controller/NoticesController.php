939849632273a5b04a705ffabf29040b7a283edd
zartos <daikerumaru@hotmail.com>
2019-04-05 10:48:06 -0500

    Se corrigio error de traducción de funcion this t cambiando signo de admiración por simbolo de porcentaje para la variable

--- src/Controller/NoticesController.php
+++ src/Controller/NoticesController.php
@@ -14,9 +14,9 @@ class NoticesController extends ControllerBase {
         
           $build = [
             '#type' => 'markup',
-         // '#markup' => $this->t('<p>Noticias ofrece un bloque en el que se mostrarán las noticias. Por eso es necesario una vez instalado el módulo, añadir el bloque de noticias desde la página de administración de bloques'.'</p><p>!block_admin_link</p>',
-         //   ['!block_admin_link' => $block_admin_link]), // Problemas con la función de traducción
-           '#markup' => '<p>Noticias ofrece un bloque en el que se mostrarán las noticias. Por eso es necesario una vez instalado el módulo, añadir el bloque de noticias desde la página de administración de bloques'.'</p><p>'.$block_admin_link.'</p>',
+          '#markup' => $this->t('<p>Noticias ofrece un bloque en el que se mostrarán las noticias. Por eso es necesario una vez instalado el módulo, añadir el bloque de noticias desde la página de administración de bloques'.'</p><p>%block_admin_link</p>',
+            ['%block_admin_link' => $block_admin_link]), // Problemas con la función de traducción
+         //  '#markup' => '<p>Noticias ofrece un bloque en el que se mostrarán las noticias. Por eso es necesario una vez instalado el módulo, añadir el bloque de noticias desde la página de administración de bloques'.'</p><p>'.$block_admin_link.'</p>',
          //   '#markup' => $block_admin_link, //funciona
               ];
          
